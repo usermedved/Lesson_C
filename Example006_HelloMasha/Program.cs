a10fa6823a6f06179aeb0cc7d0f5ed6064e474e3
Sergei Medvedev <clip1988@yandex.ru>
2022-08-09 19:38:47 +0300

    Example 006 Hello Marina project

---
+++ Example006_HelloMasha/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите ваше имя: ");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "марина")
+{
+    Console.WriteLine("Ура, это же Марина");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+
